c1b5ce505b503f0f33dc9c45eb3979876d911da4
fluffball3 <132234302+fluffball3@users.noreply.github.com>
2023-05-02 21:08:18 +0000

    Add files via upload

--- device.mk
+++ device.mk
@@ -1,10 +1,8 @@
-# Android device tree for samsung SM-M336BU (m33x)
-
-```
 #
 # Copyright (C) 2023 The Android Open Source Project
 # Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-```
+
+LOCAL_PATH := device/samsung/m33x
