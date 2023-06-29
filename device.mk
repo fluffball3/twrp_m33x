970915bf7c9cb1bfd9e1e5d016420ed717a64d81
fluffball3 <132234302+fluffball3@users.noreply.github.com>
2023-06-29 02:10:30 +0000

    Update device.mk

--- device.mk
+++ device.mk
@@ -6,3 +6,15 @@
 #
 
 LOCAL_PATH := device/samsung/m33x
+
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
+
+# Some useful binaries
+PRODUCT_PACKAGES += shrink bxhsed
+
