75491f25b74572336b79f8e2fdd655710056f674
fluffball3 <132234302+fluffball3@users.noreply.github.com>
2023-06-29 00:55:45 +0000

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_m33x-user
-add_lunch_combo omni_m33x-userdebug
-add_lunch_combo omni_m33x-eng
+add_lunch_combo twrp_m33x-user
+add_lunch_combo twrp_m33x-userdebug
+add_lunch_combo twrp_m33x-eng
