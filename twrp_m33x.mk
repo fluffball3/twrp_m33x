c30e8f1bf1970b3a4f49c1b8a3843658e183d0f0
fluffball3 <132234302+fluffball3@users.noreply.github.com>
2023-06-29 02:08:02 +0000

    Update twrp_m33x.mk

--- twrp_m33x.mk
+++ twrp_m33x.mk
@@ -1,26 +1,48 @@
 #
-# Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2022 The Android Open Source Project
 #
-# SPDX-License-Identifier: Apache-2.0
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
-# Inherit from those products. Most specific first.
+# Release name
+PRODUCT_RELEASE_NAME := m33x
+
+# Inherit from common AOSP config
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Enable project quotas and casefolding for emulated storage without sdcardfs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
-# Inherit from m33x device
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit device configuration
 $(call inherit-product, device/samsung/m33x/device.mk)
 
-PRODUCT_DEVICE := m33x
-PRODUCT_NAME := twrp_m33x
-PRODUCT_BRAND := samsung
-PRODUCT_MODEL := SM-M336BU
-PRODUCT_MANUFACTURER := samsung
+# Charger
+PRODUCT_PACKAGES += \
+    charger_res_images
 
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/samsung/m33x/recovery/root,recovery/root)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := twrp_m33x
+PRODUCT_DEVICE := m33x
+PRODUCT_MODEL := SM-M336BU
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
